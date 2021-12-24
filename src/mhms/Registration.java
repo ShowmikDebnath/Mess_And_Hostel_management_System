83b7348ce6afb097be6e82af90de0c42d473cf67
roniach019 <roniach019@gmail.com>
2021-12-24 12:31:03 +0600

    login reg

--- src/mhms/Registration.java
+++ src/mhms/Registration.java
@@ -1,33 +1,127 @@
 package mhms;
+import java.sql.*;
 import java.awt.*;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.*;
-public class Registration extends JFrame {
+import javax.swing.border.TitledBorder;
+public class Registration extends JFrame implements ActionListener{
+
+    JFrame frame;
+    JPanel signup_panel, front, registration;
+    JPanel login_panel;
+    JLabel signup_username, welcome;
+    JLabel signup_email;
+    JLabel signup_password, signup_confirm_password;
+    JLabel login_email;
+    JLabel login_password;
+    JTextField tf_signup_username;
+    JTextField tf_signup_email;
+    JPasswordField tf_signup_password;
+    JPasswordField tf_signup_confirm_password;
+    JTextField tf_login_email;
+    JPasswordField tf_login_password;
+    JButton btn_signup, btn_login, login, signup;
+    Boolean isLogined;
     public Registration()
     {
-        JPanel p1panel = new JPanel();              // all component
-        JButton login_button = new JButton("Log in");
-        JButton signin_button = new JButton("Sign in");
+        frame = new JFrame();
+        signup_panel = new JPanel();
+        front = new JPanel();
+        registration = new JPanel();
+        login_panel = new JPanel();
+        signup_username = new JLabel("User Name : ");
+        welcome = new JLabel("Welcome to  Bus Ticket Reservation System");
+        signup_email = new JLabel("Email : ");
+        signup_password = new JLabel("Password : ");
+        signup_confirm_password = new JLabel("Confirm "
+                + "Password : ");
+        tf_signup_username = new JTextField();
+        tf_signup_email = new JTextField();
+        tf_signup_password = new JPasswordField();
+        tf_signup_confirm_password = new JPasswordField();
+        btn_signup = new JButton("Sign Up");
+        signup = new JButton("Sign Up");
+        login = new JButton("Login");
 
-        p1panel.setBounds(2,2,200,200);     // all setBounds
-        login_button.setBounds(2,2,40,50);
-        signin_button.setBounds(2,2,40,50);
+        login_email = new JLabel("Email:");
+        login_password = new JLabel("Password :");
+        tf_login_email = new JTextField();
+        tf_login_password = new JPasswordField();
+        btn_login = new JButton("Login");
+        signup_username.setBounds(10, 100, 120, 30);
+        signup_email.setBounds(10, 140, 120, 30);
+        signup_password.setBounds(10, 180, 120, 30);
+        signup_confirm_password.setBounds(10, 220, 180, 30);
+        tf_signup_username.setBounds(180, 100, 120, 30);
+        tf_signup_email.setBounds(180, 140, 120, 30);
+        tf_signup_password.setBounds(180, 180, 120, 30);
+        tf_signup_confirm_password.setBounds(180, 220, 120, 30);
+        btn_signup.setBounds(180, 300, 80, 40);
 
-        p1panel.setBackground(Color.red);               // setBackground
-        login_button.setBackground(Color.blue);
-        signin_button.setBackground(Color.CYAN);
+        login_email.setBounds(10, 100, 120, 30);
+        login_password.setBounds(10, 140, 120, 30);
+        //tf_signup_username.setBounds(80, 100, 120, 30);
+        tf_login_email.setBounds(80, 100, 120, 30);
+        tf_login_password.setBounds(80, 140, 120, 30);
+        btn_login.setBounds(100, 220, 80, 40);
 
+        login.setBounds(200, 400, 80, 40);
+        signup.setBounds(400, 400, 80, 40);
+        login.setForeground(Color.blue);
+        signup.setForeground(Color.blue);
+        welcome.setBounds(30, 50, 700, 200);
+        welcome.setFont(new Font("Jokerman", Font.PLAIN, 26));
+        welcome.setForeground(Color.BLUE);
+        registration.add(welcome);
+        registration.add(login);
+        registration.add(signup);
 
-        p1panel.add(login_button,BorderLayout.CENTER);
-        p1panel.add(signin_button);
+        signup_panel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "SIGN UP FORM", TitledBorder.CENTER, TitledBorder.TOP));
+        login_panel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "LOGIN FORM", TitledBorder.CENTER, TitledBorder.TOP));
 
-        //p1panel.setLayout(new FlowLayout());
+        signup_panel.add(signup_username);
+        signup_panel.add(tf_signup_username);
+        signup_panel.add(signup_email);
+        signup_panel.add(tf_signup_email);
+        signup_panel.add(signup_password);
+        signup_panel.add(tf_signup_password);
+        signup_panel.add(signup_confirm_password);
+        signup_panel.add(tf_signup_confirm_password);
+        signup_panel.add(btn_signup);
+        login_panel.add(login_email);
+        login_panel.add(login_password);
+        login_panel.add(tf_login_email);
+        login_panel.add(tf_login_password);
+        login_panel.add(btn_login);
 
-        add(p1panel);
+        signup_panel.setLayout(null);
+        login_panel.setLayout(null);
+        registration.setLayout(null);
+        front.setLayout(null);
+        front.setBackground(Color.BLUE);
 
-        setSize(900,700);
-        setLayout(null);
-        setVisible(true);
-        setDefaultCloseOperation(3);
-        setLocationRelativeTo(null);
+        frame.add(front);
+        frame.add(registration);
+
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        frame.setSize(600, 400);
+        frame.setLayout(new GridLayout(1, 2));
+        frame.setTitle("Welcome Page");
+
+        frame.setVisible(true);
+
+        btn_signup.addActionListener(this);
+        btn_login.addActionListener(this);
+        login.addActionListener(this);
+        signup.addActionListener(this);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
